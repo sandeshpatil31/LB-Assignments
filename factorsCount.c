88e6f2c073d6d8430effd523ec1d15585502c939
Sandesh Patil <115943497+sandeshpatil31@users.noreply.github.com>
2024-07-16 20:36:07 +0530

    Update factorsCount.c

--- factorsCount.c
+++ factorsCount.c
@@ -3,35 +3,35 @@ factors of a given number */
  
 #include<stdio.h>
 
-int factorCount(int iNum)          // calling function
+int countFactors(int iNum)          // calling function
 {
 
 	 int iCnt=0;
-	 int factCount=0;
+	 int iFactCount=0;
 	 for (int iCnt = 1; iCnt<=iNum/2; iCnt++)
 	 {
 	 	if(iNum%iCnt==0)
 	 	{
-	 		factCount++;
+	 		iFactCount++;
 		}
 
 	 }
 	 
-	 return factCount + 1;                 // return factCount to main caller fun
+	 return iFactCount ;                 // return factCount to main caller fun
 
 }
 
 int main()
 {
 	int iNum=0;
-	int factCount=0;
+	int iFactCount=0;
 
 	printf("Enter the Number:\n");  
 	scanf("%d",&iNum);                  // accpet user input
 
-	factCount=factorCount(iNum);    // caller function 
+	iFactCount=countFactors(iNum);    // caller function 
 
-	printf(" %d \n",factCount);
+	printf(" %d \n",iFactCount);
 
 	return 0;
 }
